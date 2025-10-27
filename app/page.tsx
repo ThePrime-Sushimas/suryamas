f81c9c6bbd850a1cb75f6ba3952abb6fc2fb49ee
theprime-sushimas <suryamaspratama01@gmail.com>
2025-10-27 21:12:20 +0700

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,13 @@ export default function Home() {
         >
           Go to Employees
         </Link>
-      </div>
+        <Link
+          href="/master/branches"
+          className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors"
+          >
+            Go to Branches
+        </Link>
+        </div>
     </div>
   );
 }
