b166d1007f5419bfdfe4ccb41d0dcc633ed1d19b
ThePrime-Sushimas <sushimas@example.com>
2026-01-12 21:23:00 +0700

    COMMIT

--- frontend/src/features/accounting/index.ts
+++ frontend/src/features/accounting/index.ts
@@ -2,11 +2,11 @@
 export * from './shared/accounting.types'
 export * from './shared/accounting.constants'
 
-// Chart of Accounts exports
-export * from './chart-of-accounts'
-
-// Future exports for other modules
-// export * from './accounting-purposes'
-// export * from './accounting-purpose-accounts'
-// export * from './journals'
-// export * from './ledger-entries'
+// Chart of Accounts exports - explicit re-exports to avoid conflicts
+export { chartOfAccountsApi } from './chart-of-accounts/api/chartOfAccounts.api'
+export { useChartOfAccountsStore } from './chart-of-accounts/store/chartOfAccounts.store'
+export { default as ChartOfAccountsPage } from './chart-of-accounts/pages/ChartOfAccountsPage'
+export { default as CreateChartOfAccountPage } from './chart-of-accounts/pages/CreateChartOfAccountPage'
+export { default as EditChartOfAccountPage } from './chart-of-accounts/pages/EditChartOfAccountPage'
+export { default as ChartOfAccountDetailPage } from './chart-of-accounts/pages/ChartOfAccountDetailPage'
+export type * from './chart-of-accounts/types/chart-of-account.types'
